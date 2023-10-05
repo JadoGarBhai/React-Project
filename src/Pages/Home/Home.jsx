00dcc9bac35921060e1b5e30ff191db435ec3aa0
JadoGarBhai <sikandernawaz35@gmail.com>
2023-10-05 23:04:59 +0500

    'home'

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer/Footer";
 import "./Home.css";
-import "./Home";
 
 const Home = () => {
+  // TEXT SLIDER STARTED.
+
+  useEffect(() => {
+    // Your text slider code here
+    let TypingSpeed = 60;
+    let NxtMsgDelay = 2000;
+    let CharacterPos = 0;
+    let MsgBuffer = "";
+    let MsgIndex = 0;
+    let delay;
+    let id = document.getElementById("typing");
+    let messages = [
+      "Welcome To Our Website !",
+      "Tutor Finder",
+      "The Best Way To Learn From Home.",
+    ];
+
+    const StartTyping = () => {
+      if (CharacterPos !== messages[MsgIndex].length) {
+        MsgBuffer += messages[MsgIndex].charAt(CharacterPos);
+        id.innerHTML =
+          MsgBuffer +
+          (CharacterPos !== messages[MsgIndex].length - 1 ? "_" : "");
+        delay = TypingSpeed;
+        id.scrollTop = id.scrollHeight;
+      } else {
+        delay = NxtMsgDelay;
+        MsgBuffer = "";
+        CharacterPos = -1;
+        if (MsgIndex !== messages.length - 1) {
+          MsgIndex++;
+        } else {
+          MsgIndex = 0;
+        }
+      }
+      CharacterPos++;
+      setTimeout(StartTyping, delay);
+    };
+
+    StartTyping();
+  }, []);
+
+  // TEXT SLIDER ENDED.
+
   return (
     <>
       <Navbar />
@@ -12,7 +55,7 @@ const Home = () => {
       {/* <!-- SLIDER START --> */}
       <div className="bgImg">
         <div className="container">
-          <div id="typing-text" className="gradient"></div>
+          <div id="typing" className="gradient"></div>
         </div>
       </div>
 
@@ -49,7 +92,7 @@ const Home = () => {
         {/* <!-- FIRST BOX ENDED --> */}
 
         {/* <!-- SECOND BOX STARTED --> */}
-        <div className="mainBox1" style="background-color: #f6f6f6">
+        <div className="mainBox1" style={{ backgroundColor: "#f6f6f6" }}>
           <div className="container">
             <div className="box1Wrapper d-flex justify-content-between align-items-center col-12">
               <div className="textSection1 col-6">
