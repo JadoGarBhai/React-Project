0ae3708f76e42e868dd32b4563f4da1414b78c3c
JadoGarBhai <sikandernawaz35@gmail.com>
2023-10-12 22:16:48 +0500

    'Frontend'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Home from "./Pages/Home/Home";
 import Routing from "./Routing/Routing";
 
--- src/Frontend/Home/Home.jsx
+++ src/Frontend/Home/Home.jsx
@@ -1,53 +1,8 @@
-import React, { useEffect } from "react";
 import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer/Footer";
 import "./Home.css";
 
 const Home = () => {
-  // TEXT SLIDER STARTED.
-
-  useEffect(() => {
-    // Your text slider code here
-    let TypingSpeed = 60;
-    let NxtMsgDelay = 2000;
-    let CharacterPos = 0;
-    let MsgBuffer = "";
-    let MsgIndex = 0;
-    let delay;
-    let id = document.getElementById("typing");
-    let messages = [
-      "Welcome To Our Website !",
-      "Tutor Finder",
-      "The Best Way To Learn From Home.",
-    ];
-
-    const StartTyping = () => {
-      if (CharacterPos !== messages[MsgIndex].length) {
-        MsgBuffer += messages[MsgIndex].charAt(CharacterPos);
-        id.innerHTML =
-          MsgBuffer +
-          (CharacterPos !== messages[MsgIndex].length - 1 ? "_" : "");
-        delay = TypingSpeed;
-        id.scrollTop = id.scrollHeight;
-      } else {
-        delay = NxtMsgDelay;
-        MsgBuffer = "";
-        CharacterPos = -1;
-        if (MsgIndex !== messages.length - 1) {
-          MsgIndex++;
-        } else {
-          MsgIndex = 0;
-        }
-      }
-      CharacterPos++;
-      setTimeout(StartTyping, delay);
-    };
-
-    StartTyping();
-  }, []);
-
-  // TEXT SLIDER ENDED.
-
   return (
     <>
       <Navbar />
